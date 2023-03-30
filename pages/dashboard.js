433e585b86cc7e0abdc2973458c8d155ee78d991
Hardik Desai <124128351+hardikd-zignuts@users.noreply.github.com>
2023-03-30 10:18:05 +0530

    fix: :sparkles: signIn, signOut, getSession(), status

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSession, signIn, getSession } from "next-auth/react"
 
 const Dashboard = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const securePage = async () => {
+      const session = await getSession()
+      if (!session) {
+        signIn()
+      } else {
+        setLoading(false)
+      }
+    }
+    securePage()
+  }, []);
+
+  if (loading) {
+    return (
+      <h1>Loading........</h1>
+    )
+  }
   return (
     <div>
       <h2>Dashboard Page</h2>
